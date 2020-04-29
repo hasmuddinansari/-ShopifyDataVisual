d9c605c6f21d9e4d926d52e830f8280a73515649
Hasmuddin <mdaliansari33@gmail.com>
2020-04-29 12:51:28 +0530

    final

--- src/components/Charts/ChartComponent/__test__/Chart.test.js
+++ src/components/Charts/ChartComponent/__test__/Chart.test.js
@@ -1,8 +1,15 @@
 import React from "react"
 import ReactDom from "react-dom"
 import Chart from "../Chart"
+import { render } from '@testing-library/react'
 
 it("Chart rendering without crashing", () => {
     const div = document.createElement("div")
     ReactDom.render(<Chart />, div)
+})
+
+it("renders chart correctly ", () => {
+    const { getByTestId } = render(<Chart />)
+    getByTestId("Line")
+    getByTestId("Bar")
 })
